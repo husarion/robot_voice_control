5da6ca02fa5fd3d959efc6fac9f140ae2beced71
Łukasz Mitka <luk.mitka@gmail.com>
2020-09-09 09:54:39 +0200

    Install script update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install -r $(rospack find voice_control)/../requirements.txt --user
+python -m pip install -r $(rospack find voice_control)/../requirements.txt --user
 
 sudo apt install openssl
 
@@ -9,11 +9,12 @@ cd $(rospack find voice_webserver)/src/
 ls | grep -w 'deepspeech-0.6.0-models.tar.gz' && tar -xzf ./deepspeech-0.6.0-models.tar.gz
 
 yarn install 
-mkdir cert ; cd ./cert
+mkdir -p cert ; cd ./cert
 
 hostname=$1
 gpu=$2
-python3 $(rospack find voice_webserver)/src/scripts/vw_config.py --update_hostname ${hostname:-"localhost"} --gpu ${gpu:-1} 
+dd if=/dev/urandom of=~/.rnd bs=256 count=1
+python $(rospack find voice_webserver)/src/scripts/vw_config.py --update_hostname ${hostname:-"localhost"} --gpu ${gpu:-1} 
     
 openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out cert.crt -keyout key.key -subj "/C=PL/ST=Krakow/L=Krakow/O=Husarion/OU=Tutorials/CN=localhost"
 cat key.key cert.crt > server.pem
